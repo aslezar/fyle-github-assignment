39182bbbebdd4f39a122ca2ccb0f65de420ea7be
aslezar <97354675+aslezar@users.noreply.github.com>
2024-01-19 20:44:03 +0530

    Refactor form submission in index.html and add font size to repo heading in userStyle.css

--- script/script.js
+++ script/script.js
@@ -1,8 +1,14 @@
+document
+	.getElementById("githubForm")
+	.addEventListener("submit", function (event) {
+		event.preventDefault(); // Prevent the default form submission
+		redirectToUserDetails();
+	});
 
 function redirectToUserDetails() {
 	const username = document.getElementById("username").value;
 	if (username.trim() !== "") {
-		window.location.href = `/user.html?username=${username}`;
+		window.location.href = `user.html?username=${username}`;
 	} else {
 		alert("Please enter a valid GitHub username.");
 	}
